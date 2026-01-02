92d783e244c0db83711331c73bf4468c3e963642
sandihermawan505 <sandihermawan505@gmail.com>
2026-01-02 18:04:20 +0700

    first commit

---
+++ database/migrations/2026_01_02_104218_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code')->unique();
+            $table->text('title');
+            $table->longtext('description');
+            $table->enum('status', ['open', 'omprogress', 'resolved', 'rejected'])->default('open');
+            $table->enum('priority', ['low', 'medium', 'high']);
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
