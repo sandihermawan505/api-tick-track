6217c90b19d1517f1c454449e70a44d1c77d2630
sandihermawan505 <sandihermawan505@gmail.com>
2026-01-04 18:09:40 +0700

    add ticket and user model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'code',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'completed_at'
+    ];
+
+    public functtion user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
